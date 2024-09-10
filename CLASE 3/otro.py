628e5847ae50460342671de5aa995a6201944566
grennarrow3212@gmail.com <>
2024-09-10 09:34:44 -0500

    primer commit

---
+++ CLASE 3/otro.py
@@ -0,0 +1,8 @@
+import index
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
+result = index.suma(num1,num2)
+print(result)
+                                                   
+
+                    
